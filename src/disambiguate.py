2dacb43726fa30079e7c3c1ffc1bc66cb4df786a
lavallone <lavalle.1838492@studenti.uniroma1.it>
2024-06-22 11:58:27 +0200

    prova

--- src/disambiguate.py
+++ src/disambiguate.py
@@ -174,10 +174,7 @@ def _process(output_file_path:str, subtask:str, prompt_type:str, prompt_addition
         full_model_name = shortcut_model_name2full_model_name[shortcut_model_name]
         tokenizer = AutoTokenizer.from_pretrained(full_model_name, trust_remote_code=True)
         tokenizer.pad_token = tokenizer.eos_token
-        model = AutoModelForCausalLM.from_pretrained(full_model_name, trust_remote_code=True, torch_dtype=torch.float16, attn_implementation="flash_attention_2").cuda()
-        if getattr(model, "_supports_flash_attn_2") == True:
-            #model.config.attn_implementation = "flash_attention_2"
-            print("AAHHAAJAJAKAABANAJNA \n\n\n\n")
+        model = AutoModelForCausalLM.from_pretrained(full_model_name, trust_remote_code=True, torch_dtype=torch.float16, attn_implementation="flash_attention_2", device="cuda")
         pipe = pipeline("text-generation", model=model, device="cuda", tokenizer=tokenizer, pad_token_id=tokenizer.eos_token_id, max_new_tokens=25)
     
     with open(f"{output_file_path}/output.txt", "a") as fa_txt, open(f"{output_file_path}/output.json", "w") as fw_json:
