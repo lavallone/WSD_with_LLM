d8841279ed2f4658cb002774457b7876d0d2e8b0
lavallone <lavalle.1838492@studenti.uniroma1.it>
2024-06-22 12:15:11 +0200

    s

--- src/disambiguate.py
+++ src/disambiguate.py
@@ -240,8 +240,9 @@ def _process_wic(output_file_path:str, subtask:str, prompt_type:str, prompt_addi
             instance_id = instance_data["id"]
             
             prompt = _generate_prompt([instance_data, instance_gold], subtask, prompt_type, prompt_addition, approach)
-
-            answer = pipe(prompt)[0]["generated_text"].replace(prompt, "").replace("\n", "").strip()
+            messages = [{"role": "user", "content": "How many helicopters can a human eat in one sitting?"}]
+            prompt_template = tokenizer.apply_chat_template(messages, tokenize=True, add_generation_prompt=True, return_tensors="pt")
+            answer = pipe(prompt_template)[0]["generated_text"].replace(prompt_template, "").replace("\n", "").strip()
             
             fa_txt.write(f"{instance_id}\t{answer}\n")
             fa_txt.flush()
