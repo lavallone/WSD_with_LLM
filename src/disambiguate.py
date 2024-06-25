52254818418ac40026f915f19967f735056a8613
lavallone <lavalle.1838492@studenti.uniroma1.it>
2024-06-25 17:52:24 +0200

    prova

--- src/disambiguate.py
+++ src/disambiguate.py
@@ -186,7 +186,7 @@ def _process(output_file_path:str, subtask:str, prompt_type:str, prompt_addition
         tokenizer = AutoTokenizer.from_pretrained(full_model_name, trust_remote_code=True)
         tokenizer.pad_token = tokenizer.eos_token
         if shortcut_model_name == "falcon" : model = FalconForCausalLM.from_pretrained(full_model_name, trust_remote_code=True, torch_dtype=torch.float16, attn_implementation="flash_attention_2").cuda()
-        else: model = AutoModelForCausalLM.from_pretrained(full_model_name, trust_remote_code=True, torch_dtype=torch.float16, attn_implementation="flash_attention_2").cuda()
+        else: model = AutoModelForCausalLM.from_pretrained(full_model_name, trust_remote_code=True, torch_dtype=torch.float16).cuda()#, attn_implementation="flash_attention_2").cuda()
         pipe = pipeline("text-generation", model=model, device="cuda", tokenizer=tokenizer, pad_token_id=tokenizer.eos_token_id, max_new_tokens=25)
     
     with open(f"{output_file_path}/output.txt", "a") as fa_txt, open(f"{output_file_path}/output.json", "w") as fw_json:
